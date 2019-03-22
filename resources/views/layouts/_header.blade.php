20557acc90c7a2957ed077629201d06e3a4ce903
xiehang <1151061165@qq.com>
2019-03-22 22:01:54 +0800

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
-<nav>
+<nav class="navbar navbar-expand-lg navbar-dark dg-dark">
   <div class = "container">
-    <a class="nav-bar-brand" href="/"></a>
+    <a class="nav-bar-brand" href="{{ route('home')}}">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" href="/help"></li>
+      <li class="nav-item"><a class="nav-link" href="{{route('help')}}">帮助</li>
       <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
     </ul>
   </div>
