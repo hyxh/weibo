01c56450c39d3340030e228a963de6be985c6222
xiehang <1151061165@qq.com>
2019-03-22 21:53:52 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,9 @@
+<nav>
+  <div class = "container">
+    <a class="nav-bar-brand" href="/"></a>
+    <ul class="navbar-nav justify-content-end">
+      <li class="nav-item"><a class="nav-link" href="/help"></li>
+      <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+    </ul>
+  </div>
+</nav>
