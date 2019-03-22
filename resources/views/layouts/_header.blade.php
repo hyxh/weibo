f59b93ad4d80a690fa27c16ffd5a5d223c30ee90
hyaaa <1151061164@qq.com>
2019-03-22 15:28:37 +0000

    用户注册界面

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-dark dg-dark">
-  <div class = "container">
-    <a class="nav-bar-brand" href="{{ route('home')}}">Weibo App</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container ">
+    <a class="navbar-brand" href="/">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" href="{{route('help')}}">帮助</li>
+      <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
       <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
     </ul>
   </div>
